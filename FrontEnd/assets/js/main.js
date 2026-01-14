594c9c97dbb6d6d1ea3fe2ed13e756a13a2acd9d
Mahlet HAILE GERRU <m.hlufafera@gmail.com>
2026-01-14 14:59:56 +0100

    Ajout de la fonctionnalité de logout

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,4 +1,19 @@
 getWorks();
 getCategories();
 
+function logout() {
+  localStorage.removeItem("token");
+  window.location.href = "index.html";
+}
 const token = localStorage.getItem("token");
+
+if (token) {
+  const loginElement = document.querySelector("#login-page");
+  loginElement.innerText = "logout";
+  loginElement.href = "#";
+
+  loginElement.addEventListener("click", (e) => {
+    e.preventDefault();
+    logout();
+  });
+}
