659486dcdc68a7bf8916b1386024be8f68ea6d02
Mahlet HAILE GERRU <m.hlufafera@gmail.com>
2026-01-15 10:41:57 +0100

    Ajouter l'icon de  mod édition

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,5 +1,4 @@
 getWorks();
-getCategories();
 
 function logout() {
   localStorage.removeItem("token");
@@ -8,6 +7,7 @@ function logout() {
 const token = localStorage.getItem("token");
 
 if (token) {
+  //Connecté
   const loginElement = document.querySelector("#login-page");
   loginElement.innerText = "logout";
   loginElement.href = "#";
@@ -16,4 +16,14 @@ if (token) {
     e.preventDefault();
     logout();
   });
+  // pour ajouter d'icon de mod édition
+  const iconHtml = `<i class="fa-regular fa-pen-to-square"></i>`;
+  const divElement = document.createElement("div");
+  divElement.classList.add("icon-div");
+  divElement.innerHTML = iconHtml + "Mode édition";
+  const body = document.querySelector("body");
+  body.prepend(divElement);
+} else {
+  // Déconnecté
+  getCategories();
 }
